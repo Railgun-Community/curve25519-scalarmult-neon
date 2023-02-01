77290ff26579d0bb932ec66aeb235edb446717b1
Andre Staltz <andre@staltz.com>
2023-02-01 13:01:25 +0200

    harden postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,28 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-async function mkdirp(folder) {
-  if (fs.existsSync(folder)) return;
-  try {
-    fs.mkdirSync(folder);
-  } catch (err) {}
+// npm_config_platform exists only when nodejs-mobile is building our module
+const platform = process.env['npm_config_platform'];
+
+// On iOS nodejs-mobile we need index.node to be a folder that
+// will be converted to a .framework
+const outputPath = path.join(__dirname, 'dist', 'index.node');
+const tempPath = path.join(__dirname, 'dist', 'index.node.tmp');
+if (platform === 'ios' && fs.existsSync(outputPath)) {
+  fs.renameSync(outputPath, tempPath);
+  fs.mkdirSync(outputPath);
+  fs.renameSync(tempPath, path.join(outputPath, 'index'));
 }
-
-(async function main() {
-  // npm_config_platform exists only when nodejs-mobile is building our module
-  const platform = process.env['npm_config_platform'];
-
-  // On iOS nodejs-mobile we need index.node to be a folder that
-  // will be converted to a .framework
-  if (platform === 'ios') {
-    await fs.promises.rename(
-      path.join(__dirname, 'dist', 'index.node'),
-      path.join(__dirname, 'dist', 'index'),
-    );
-    mkdirp(path.join(__dirname, 'dist', 'index.node'));
-    await fs.promises.rename(
-      path.join(__dirname, 'dist', 'index'),
-      path.join(__dirname, 'dist', 'index.node', 'index'),
-    );
-  }
-})();
