58d6d4a7919938f32fa13228e1d95dada6e271d0
Andre Staltz <andre@staltz.com>
2023-03-06 14:18:02 +0200

    dist was a mistake in postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -6,8 +6,8 @@ const platform = process.env['npm_config_platform'];
 
 // On iOS nodejs-mobile we need index.node to be a folder that
 // will be converted to a .framework
-const outputPath = path.join(__dirname, 'dist', 'index.node');
-const tempPath = path.join(__dirname, 'dist', 'index.node.tmp');
+const outputPath = path.join(__dirname, 'index.node');
+const tempPath = path.join(__dirname, 'index.node.tmp');
 if (platform === 'ios' && fs.existsSync(outputPath)) {
   fs.renameSync(outputPath, tempPath);
   fs.mkdirSync(outputPath);
